f1728602aded2fb4f2990bb1e4ea6f7bb510575c
Thien Nguyen <112293169+ThienNg65@users.noreply.github.com>
2023-05-26 16:07:25 +0700

    Add files via upload

---
+++ screens/ticket_screen.dart
@@ -0,0 +1,134 @@
+import 'package:booktickets/screens/ticket_view.dart';
+import 'package:booktickets/utils/app_info_list.dart';
+import 'package:booktickets/utils/app_layout.dart';
+import 'package:booktickets/utils/app_style.dart';
+import 'package:booktickets/widges/column_layout.dart';
+import 'package:booktickets/widges/ticket_tabs.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../widges/layout_builder_widget.dart';
+
+class TicketScreen extends StatelessWidget {
+  const TicketScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: Stack(
+        children: [
+          ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppLayout.getHeight(20),
+              vertical: AppLayout.getHeight(20),
+            ),
+            children: [
+              Gap(AppLayout.getHeight(40)),
+              Text(
+                "Tickets",
+                style: Styles.headLineStyle1,
+              ),
+              Gap(AppLayout.getHeight(20)),
+              const AppTicketTabs(firstTab: "Upcoming", secondTab: "Previous"),
+              Gap(AppLayout.getHeight(20)),
+              Container(
+                padding: EdgeInsets.only(left: AppLayout.getHeight(15)),
+                child: TicketView(
+                  ticket: ticketList[0],
+                  isColor: true,
+                ),
+              ),
+              SizedBox(height: 1),
+              Container(
+                color: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        AppColumnLayout(
+                          firstText: 'Flutter DB',
+                          secondText: 'Passenger',
+                          alignment: CrossAxisAlignment.start,
+                          isColor: false,
+                        ),
+                        AppColumnLayout(
+                          firstText: '5221 364896',
+                          secondText: 'passport',
+                          alignment: CrossAxisAlignment.end,
+                          isColor: false,
+                        ),
+                      ],
+                    ),
+                    Gap(AppLayout.getHeight(20)),
+                    const AppLayoutBuilderWidget(
+                      sections: 15,
+                      isColor: false,
+                      width: 5,
+                    ),
+                    Gap(AppLayout.getHeight(20)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        AppColumnLayout(
+                          firstText: '32552523 235235',
+                          secondText: 'Number of E-ticket',
+                          alignment: CrossAxisAlignment.start,
+                          isColor: false,
+                        ),
+                        AppColumnLayout(
+                          firstText: 'B2005893',
+                          secondText: 'Booking code',
+                          alignment: CrossAxisAlignment.end,
+                          isColor: false,
+                        ),
+                      ],
+                    ),
+                    Gap(AppLayout.getHeight(20)),
+                    const AppLayoutBuilderWidget(
+                      sections: 15,
+                      isColor: false,
+                      width: 5,
+                    ),
+                    Gap(AppLayout.getHeight(20)),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  "assets/images/visa.png",
+                                  scale: 11,
+                                ),
+                                Text(
+                                  " *** 2462",
+                                  style: Styles.headLineStyle3,
+                                )
+                              ],
+                            ),
+                            Gap(5),
+                            Text(
+                              "Payment method",
+                              style: Styles.headLineStyle4,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
